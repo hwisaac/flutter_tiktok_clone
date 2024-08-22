aad5a0b6248686309cffc6e5336171130a51bdf6
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-08-22 23:46:40 +0900

    rotation transition

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -10,18 +10,53 @@ class ActivityScreen extends StatefulWidget {
   State<ActivityScreen> createState() => _ActivityScreenState();
 }
 
-class _ActivityScreenState extends State<ActivityScreen> {
+class _ActivityScreenState extends State<ActivityScreen>
+    with SingleTickerProviderStateMixin {
   final List<String> _notification = List.generate(20, (index) => "${index}h");
+  late final AnimationController _animationController =
+      AnimationController(vsync: this, duration: Duration(milliseconds: 200));
+
+  late final Animation<double> _animation = Tween(
+    begin: 0.0,
+    end: 0.5,
+  ).animate(_animationController);
+
   void _onDismissed(String notification) {
     _notification.remove(notification);
     setState(() {});
+  }
+
+  void _onTitleTap() {
+    if (_animationController.isCompleted) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward(); // 애니메이션 플레이
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("All activity"),
+        title: GestureDetector(
+          onTap: _onTitleTap,
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+            Text("All activity"),
+            Gaps.h2,
+            RotationTransition(
+              turns: _animation,
+              child: const FaIcon(
+                FontAwesomeIcons.chevronDown,
+                size: Sizes.size14,
+              ),
+            )
+          ]),
+        ),
       ),
       body: ListView(
         children: [
