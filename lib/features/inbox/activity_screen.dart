5b9cdc2df6f9bd38188d6e530519c90799aee602
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-08-22 21:26:32 +0900

    dismissible

--- lib/features/inbox/activity_screen.dart
+++ lib/features/inbox/activity_screen.dart
@@ -13,64 +13,101 @@ class ActivityScreen extends StatelessWidget {
         title: const Text("All activity"),
       ),
       body: ListView(
-        padding: EdgeInsets.symmetric(horizontal: Sizes.size20),
         children: [
           Gaps.v14,
-          Text(
-            'New',
-            style: TextStyle(
-              fontSize: Sizes.size14,
-              color: Colors.grey.shade500,
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: Sizes.size10,
+            ),
+            child: Text(
+              'New',
+              style: TextStyle(
+                fontSize: Sizes.size14,
+                color: Colors.grey.shade500,
+              ),
             ),
           ),
           Gaps.v14,
-          ListTile(
-            contentPadding: EdgeInsets.zero,
-            leading: Container(
-              width: Sizes.size52,
-              decoration: BoxDecoration(
+          Dismissible(
+            background: Container(
+              alignment: Alignment.centerLeft,
+              color: Colors.green,
+              child: const Padding(
+                padding: EdgeInsets.only(
+                  left: Sizes.size10,
+                ),
+                child: FaIcon(
+                  FontAwesomeIcons.checkDouble,
+                  color: Colors.white,
+                  size: Sizes.size16,
+                ),
+              ),
+            ),
+            secondaryBackground: Container(
+              alignment: Alignment.centerRight,
+              color: Colors.red,
+              child: const Padding(
+                padding: EdgeInsets.only(
+                  right: Sizes.size10,
+                ),
+                child: FaIcon(
+                  FontAwesomeIcons.trashCan,
+                  color: Colors.white,
+                  size: Sizes.size16,
+                ),
+              ),
+            ),
+            key: const Key("x"),
+            child: ListTile(
+              leading: Container(
+                width: Sizes.size40,
+                decoration: BoxDecoration(
+                    
                   shape: BoxShape.circle,
                   color: Colors.white,
                   border: Border.all(
                     color: Colors.grey.shade400,
                     width: Sizes.size1,
-                  )),
-              child: const Center(
-                child: FaIcon(
-                  FontAwesomeIcons.bell,
-                  color: Colors.black,
+                  ),
+                ),
+                child: const Center(
+                  child: FaIcon(
+                    FontAwesomeIcons.bell,
+                    color: Colors.black,
+                    size: Sizes.size16,
+                  ),
                 ),
               ),
-            ),
-            title: RichText(
-              text: TextSpan(
-                text: "Account updates:",
-                style: const TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: Colors.black,
-                  fontSize: Sizes.size16,
+              title: RichText(
+                text: TextSpan(
+                  text: "Account updates:",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Colors.black,
+                    fontSize: Sizes.size16,
+                  ),
+                  children: [
+                    const TextSpan(
+                      text: " Upload longer videos",
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                    TextSpan(
+                      text: " 1h",
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ],
                 ),
-                children: [
-                  const TextSpan(
-                    text: " Upload longer videos",
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  TextSpan(
-                    text: " 1h",
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      color: Colors.grey.shade500,
-                    ),
-                  ),
-                ],
               ),
-            ),
-            trailing: FaIcon(
-              FontAwesomeIcons.chevronRight,
-              size: Sizes.size16,
-              color: Colors.grey.shade500,
+              trailing: FaIcon(
+                FontAwesomeIcons.chevronRight,
+                size: Sizes.size16,
+                color: Colors.grey.shade500,
+              ),
             ),
           ),
         ],
