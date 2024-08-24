32a0b3e0453eea00548db881ee6b35633dfd4bad
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-08-24 19:58:18 +0900

    animated list

--- lib/features/inbox/chats_screen.dart
+++ lib/features/inbox/chats_screen.dart
@@ -2,8 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
-class ChatsScreen extends StatelessWidget {
+class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
+
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+  List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(
+          milliseconds: 100,
+        ),
+      );
+      _items.add(_items.length);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,41 +32,53 @@ class ChatsScreen extends StatelessWidget {
         elevation: 1,
         title: const Text("Direct message"),
         actions: [
-          IconButton(onPressed: () {}, icon: FaIcon(FontAwesomeIcons.plus))
-        ],
-      ),
-      body: ListView(
-        padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 30,
-              child: Text('이삭'),
-              foregroundImage: NetworkImage(
-                  "https://avatars.githubusercontent.com/u/54179672?v=4"),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const Text(
-                  "hwisaac",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                Text(
-                  "2:16 PM",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: Sizes.size12,
-                  ),
-                ),
-              ],
-            ),
-            subtitle: const Text("Don't forget to make video"),
+          IconButton(
+            onPressed: _addItem,
+            icon: const FaIcon(FontAwesomeIcons.plus),
           )
         ],
+      ),
+      body: AnimatedList(
+        key: _key,
+        padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
+        itemBuilder:
+            (BuildContext context, int index, Animation<double> animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  child: Text('이삭'),
+                  foregroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/54179672?v=4"),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "$index",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "2:16 PM",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size12,
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: const Text("Don't forget to make video"),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
