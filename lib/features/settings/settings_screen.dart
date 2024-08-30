aedb538e4650082e284899d35a12b68af4296e67
hwisaac <54179672+hwisaac@users.noreply.github.com>
2024-08-31 00:18:29 +0900

    switchListTile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notification = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notification = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +24,43 @@ class SettingsScreen extends StatelessWidget {
           title: const Text("Settings"),
         ),
         body: ListView(
-          children: const [
-            AboutListTile(),
+          children: [
+            Switch(value: _notification, onChanged: _onNotificationsChanged),
+            Checkbox(
+              activeColor: Colors.black,
+              value: _notification,
+              onChanged: _onNotificationsChanged,
+            ),
+            CheckboxListTile(
+              activeColor: Colors.black,
+              value: _notification,
+              onChanged: _onNotificationsChanged,
+              title: const Text('Enable notifications'),
+            ),
+            ListTile(
+              onTap: () async {
+                final date = await showDatePicker(
+                  context: context,
+                  initialDate: DateTime.now(),
+                  firstDate: DateTime(1980),
+                  lastDate: DateTime(2030),
+                );
+                print(date);
+                final time = await showTimePicker(
+                  context: context,
+                  initialTime: TimeOfDay.now(),
+                );
+                print(time);
+                final booking = await showDateRangePicker(
+                  context: context,
+                  firstDate: DateTime(1980),
+                  lastDate: DateTime(2030),
+                );
+                print(booking);
+              },
+              title: Text("hello"),
+            ),
+            const AboutListTile(),
           ],
         ));
   }
